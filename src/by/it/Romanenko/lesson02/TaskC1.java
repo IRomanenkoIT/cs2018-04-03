dbf3055567fa5e7793bc9e10486378ab040e20b3
IRomanenkoIT <ivan.romanenko2011@gmail.com>
2018-04-07 07:57:00 -0400

    Lesson2

--- src/by/it/Romanenko/lesson02/TaskC1.java
+++ src/by/it/Romanenko/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.Romanenko.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,7 +12,22 @@ package by.it.Romanenko.lesson02;
 Sum = 26
 */
 class TaskC1 {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        int b = 1;
+        int sum = 0;
+        System.out.println("Ввод:");
+        if (sc.hasNextInt()) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            sum = a+b;
+            System.out.println("Вывод:");
+            System.out.println("Sum = " + sum);
+        } else {
+            System.out.println("Вы ввели не целое число");
+        }
+    }
 
 
 
