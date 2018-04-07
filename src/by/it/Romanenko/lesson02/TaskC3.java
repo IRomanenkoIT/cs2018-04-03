44cfc8788babf9dc2d8cd236aa74e3a78b057c46
IRomanenkoIT <ivan.romanenko2011@gmail.com>
2018-04-07 07:57:00 -0400

    Lesson2

--- src/by/it/Romanenko/lesson02/TaskC3.java
+++ src/by/it/Romanenko/lesson02/TaskC3.java
@@ -1,5 +1,7 @@
 package by.it.Romanenko.lesson02;
 
+import java.util.Scanner;
+
 /*
 Ускорение свободного падения на Земле и Марсе таково:
 Марс   3,86
@@ -30,5 +32,20 @@ package by.it.Romanenko.lesson02;
 
 */
 class TaskC3 {
+  //  public
+  //  public static void main(String[] args) {
+  //      Scanner sc = new Scanner(System.in);
+   //     int weightEarth = 1;
+   //     System.out.println("Ввод:");
+   //     weightEarth = sc.nextInt();
+  //      System.out.println("Вывод:");
+  //      System.out.println(getWeight(888));
+ //       System.out.println(weigthMoon);
+ //   }
 
+ //   public static double getWeight(int weightEarth) {
+ //       double m = weightEarth / g;
+ //       double p = m * (g / 6);
+ //       return p;
+ //   }
 }
