0dadbcc3a9e282ebbfd57e9422feb51baa54b1c9
IRomanenkoIT <ivan.romanenko2011@gmail.com>
2018-04-07 08:05:19 -0400

    Lesson3Start

--- src/by/it/Romanenko/lesson03/TaskA2.java
+++ src/by/it/Romanenko/lesson03/TaskA2.java
@@ -21,9 +21,5 @@ Lesson 03. Task A2. Стихотворение
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
-    public static void main(String[] args) {
-        System.out.println("Мое любимое стихотворение:");
-        System.out.println("fdshd fjgfykjf ukfdhf gdgdgds \ndrhdfj gfkjgf hfdgdgdg dsgd \ndrhd fjgfkjgf hfdgdgd gdsgd \ndhdf jgfuk jgf jdhfgssgh");
-        }
 
 }
