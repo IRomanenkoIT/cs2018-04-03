6008cbab82dfb62b2c74bc1b7940e32f39b5d3e7
IRomanenkoIT <ivan.romanenko2011@gmail.com>
2018-04-09 20:14:31 -0400

    Lesson3Finish

--- src/by/it/Romanenko/lesson03/TaskC2.java
+++ src/by/it/Romanenko/lesson03/TaskC2.java
@@ -20,12 +20,23 @@ package by.it.Romanenko.lesson03;
     5. Метод sumDigitsInNumber должен правильно возвращать сумму всех цифр в числе number.
 */
 public class TaskC2 {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(5467));
+    }
 
+    public static int sumDigitsInNumber(int number) {
 
+        String s = Integer.toString(number);
 
+        char[] chars = s.toCharArray();
 
-//    public static void main(String[] args) {
-//        System.out.println(sumDigitsInNumber(5467));
-//    }
+        int result = 0;
 
+        for (int i = 0; i < chars.length; i++) {
+            result += Character.getNumericValue(chars[i]);
+        }
+
+        return result;
+
+    }
 }
